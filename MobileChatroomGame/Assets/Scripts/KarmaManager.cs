dcc98ead7a1453e99890a4c383885c1ff1494234
SirBucketT <tkpriva@icloud.com>
2025-07-30 11:04:18 +0200

    karma update

--- MobileChatroomGame/Assets/Scripts/KarmaManager.cs
+++ MobileChatroomGame/Assets/Scripts/KarmaManager.cs
@@ -1,11 +1,18 @@
+using System;
 using UnityEngine;
 
 public class KarmaManager : MonoBehaviour
 {
     [SerializeField] SoKarma karmaData;
+    
+    public bool isKarmaDepleated;
+    public bool isKarmaMaxed;
+    
 
     public void Start()
     {
+        isKarmaDepleated = false;
+        isKarmaMaxed = false;
         ResetKarma();
     }
 
@@ -17,10 +24,20 @@ public class KarmaManager : MonoBehaviour
     public void IncreaseKarma(float amount)
     {
         karmaData.CurrentKarma += amount;
+        
+        if (karmaData.CurrentKarma <= karmaData.MinKarma)
+        {
+            karmaData.CurrentKarma = karmaData.MinKarma;
+        }
     }
 
     public void DecreaseKarma(float amount)
     {
         karmaData.CurrentKarma -= amount;
+        
+        if (karmaData.CurrentKarma >= karmaData.MaxKarma)
+        {
+            karmaData.CurrentKarma = karmaData.MaxKarma;
+        }
     }
 }
