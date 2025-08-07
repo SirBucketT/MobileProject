f7c239dff6b216562192de8ef1d0c7ba156bd5df
SirBucketT <tkpriva@icloud.com>
2025-08-07 12:31:45 +0200

    Started work on firebase Login UI

---
+++ MobileChatroomGame/Assets/Scripts/UI/PreLoginUI.cs
@@ -0,0 +1,39 @@
+/*
+ * This script manages the current UI state of the login window shown to the user at the start of the game before the core User Interface
+ * where the user can select levels and play game.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreLoginUI : MonoBehaviour
+{
+    
+    [SerializeField] GameObject loginMenu, signupMenu, accountPanel;
+
+    void Start()
+    {
+        AccountPaneManager();
+    }
+
+    void AccountPaneManager()
+    {
+        accountPanel.SetActive(true);
+        loginMenu.SetActive(true);
+        signupMenu.SetActive(false);
+    }
+
+    public void SignupMenuButton()
+    {
+        loginMenu.SetActive(false);
+        signupMenu.SetActive(true);
+    }
+
+    public void ReturnToLoginMenu()
+    {
+        loginMenu.SetActive(true);
+        signupMenu.SetActive(false);
+    }
+    
+}
