0e266044426bbccc7350dd76c6b3251a7d706321
SirBucketT <tkpriva@icloud.com>
2025-08-13 11:17:59 +0200

    test ads display properly again

--- MobileChatroomGame/Assets/Scripts/Unity Ads/ShowAd.cs
+++ MobileChatroomGame/Assets/Scripts/Unity Ads/ShowAd.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShowAd : MonoBehaviour
-{ /*
+{ 
     [SerializeField] InterstitialAdExample _interstitialAd;
 
    public void DisplayAds()
     {
         _interstitialAd.ShowAd();
-    }*/
-}
+    }
+    }
