cb4328ae707060710ce05f7a27b6bc0c7e4c9911
Hunzlah <56739241+Hunzlah@users.noreply.github.com>
2025-08-13 12:00:51 +0200

    Updated Player Response system - options spawned inside chatbox

--- MobileChatroomGame/Assets/Scripts/ChatBox/ChatControllerUI.cs
+++ MobileChatroomGame/Assets/Scripts/ChatBox/ChatControllerUI.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,19 +14,29 @@ namespace ChatRoom.UI
         [SerializeField] private ChatMessageUI receivedMessagePrefab;
         [SerializeField] private ScrollRect scrollRect;
 
+        // Player Response
+        [SerializeField] private PlayerResponseUI responsePrefab;
+
+        private Action<Response> callback;
+
+        private List<GameObject> currentResponses;
+
         private float maxBubbleWidth;
 
         private void OnEnable ()
         {
             Broker.Subscribe<MessageData>(AddMessage);
+            Broker.Subscribe<PlayerResponseData>(ShowResponses);
         }
         private void OnDisable ()
         {
             Broker.Unsubscribe<MessageData>(AddMessage);
+            Broker.Unsubscribe<PlayerResponseData>(ShowResponses);
         }
         private void Start ()
         {
             maxBubbleWidth = scrollRect.content.rect.width / 1.5f;
+            currentResponses = new List<GameObject>();
         }
         public void AddMessage (MessageData data)
         {
@@ -40,6 +52,44 @@ namespace ChatRoom.UI
             scrollRect.verticalNormalizedPosition = 0f;
 
             msgGO.UpdateRects();
+        }
+
+
+        private void ShowResponses (PlayerResponseData responseData)
+        {
+            callback = responseData.callback;
+            RemoveAllResponses();
+            foreach (var response in responseData.responses)
+            {
+                var msgGO = Instantiate(responsePrefab, scrollRect.content);
+
+                msgGO.GetComponent<RectTransform>().sizeDelta = new Vector2(scrollRect.content.sizeDelta.x, msgGO.GetComponent<RectTransform>().sizeDelta.y);
+
+                msgGO.Initalize(response, maxBubbleWidth);
+                msgGO.OnResponseSelected += OnResponseSelected;
+
+                Canvas.ForceUpdateCanvases();
+                scrollRect.verticalNormalizedPosition = 1f;
+
+                msgGO.UpdateRects();
+
+                currentResponses.Add(msgGO.gameObject);
+            }
+
+        }
+        private void RemoveAllResponses ()
+        {
+            for (int i = 0; i < currentResponses.Count; i++)
+            {
+                Destroy(currentResponses[i]);
+            }
+            currentResponses.Clear();
+        }
+        private void OnResponseSelected (Response response)
+        {
+            RemoveAllResponses ();
+            callback?.Invoke(response);
+            callback = null;
         }
     }
 }
